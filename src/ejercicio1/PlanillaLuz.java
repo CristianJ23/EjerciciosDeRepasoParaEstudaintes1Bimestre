2570f998016dd13e9e06b6b7a638d1690680fab6
CristianJ23 <cristianmjr23@gmail.com>
2025-11-28 15:15:36 -0500

    first commit

---
+++ src/ejercicio1/PlanillaLuz.java
@@ -0,0 +1,23 @@
+package ejercicio1;
+
+/*Reto: Desarrolla un programa en Java que calcule el valor total a pagar por una planilla de luz.
+Instrucciones:
+Pide al usuario que ingrese el costo por kilovatio/hora (un número decimal).
+Pide al usuario que ingrese el número de kilovatios consumidos en el mes (un número entero).
+Calcula el valor a pagar usando la fórmula: Total = Costo por Kilovatio * Kilovatios Consumidos
+Muestra en pantalla el valor total a cancelar.*/
+
+import java.util.Scanner;
+
+public class PlanillaLuz {
+        Scanner entrada = new Scanner(System.in);
+        // 1. Declarar variables (costoKw, consumidos, total)
+
+        // 2. Pedir al usuario el costo por kilovatio/hora
+
+        // 3. Pedir al usuario los kilovatios consumidos
+
+        // 4. Calcular el total a pagar
+
+        // 5. Mostrar el resultado
+}
